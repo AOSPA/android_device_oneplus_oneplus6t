63b20c0d773d6c0efd4491f37a4a9eca3cf3f484
Peter Cai <peter@typeblog.net>
2019-06-21 14:15:30 +0800

    oneplus6t: Introduce in-screen fingerprint HAL
    Change-Id: I0c034def5803ab80e6158a4c4d1caf1eeaa92841

--- device.mk
+++ device.mk
@@ -23,12 +23,20 @@ $(call inherit-product-if-exists, vendor/oneplus/oneplus6t/oneplus6t-vendor.mk)
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    vendor/pa/config/permissions/vendor.pa.biometrics.fingerprint.inscreen.xml:system/etc/permissions/vendor.pa.biometrics.fingerprint.inscreen.xml
+
 # Properties
 -include $(LOCAL_PATH)/device-props.mk
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+# Fingerprint
+PRODUCT_PACKAGES += \
+    pa.biometrics.fingerprint.inscreen@1.0-service.oneplus_fajita
 
 # Inherit from sdm845-common
 $(call inherit-product, device/oneplus/sdm845-common/sdm845.mk)
