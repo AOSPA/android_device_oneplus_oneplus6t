LineageOS/android_device_oneplus_common

common: Import OnePlus HAL definitions
Change-Id: If3642b547a09d84cb8cc6101960a90da2b8579e1

interfaces/update-makefiles.sh
@@ -1,0 +1,6 @@
+#!/bin/bash
+
+source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
+
+do_makefiles_update \
+  "vendor.oneplus:device/oneplus/common/interfaces"

AOSPA/android_device_oneplus_oneplus6t

interfaces/update-makefiles.sh
@@ -1,0 +1,6 @@
+#!/bin/bash
+
+source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
+
+do_makefiles_update \
+  "vendor.oneplus:device/oneplus/oneplus6t/interfaces"